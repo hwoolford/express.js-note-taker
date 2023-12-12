f798631ae965798310ac82b7f35b0cbb163b9e0b
forxtoisate <hwoolford@gmail.com>
2023-12-12 14:31:37 -0600

    added server.js and some variables

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { writeFile } = require('fs').promises;
+const notesDB = require('./db/db.json')
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
